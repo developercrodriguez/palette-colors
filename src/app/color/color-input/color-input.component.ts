4eb81222e232c11aed9d0b19db93d48c58007978
Carlos Rodriguez Ortiz <carlos.rodriguez.otz@outlook.es>
2023-05-01 11:40:39 +0200

    Add new functionality for get the colors and see the color you have in the input

--- src/app/color/color-input/color-input.component.ts
+++ src/app/color/color-input/color-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { colorCode } from 'src/app/model/color';
 
 @Component({
@@ -6,13 +6,27 @@ import { colorCode } from 'src/app/model/color';
   templateUrl: './color-input.component.html',
   styleUrls: ['./color-input.component.css']
 })
-export class ColorInputComponent {
-  color: colorCode;
+export class ColorInputComponent implements OnInit{
+  color: string;
+
+
+  constructor() {
+    this.color = "#000000";
+  }
+  ngOnInit(): void {
+    this.color = "#000000";
+  }
 
 
   @Output('colorChanged')
   colorEmitter = new EventEmitter<String>();
   onSaveClicked(colorCode:string) {
     this.colorEmitter.emit(colorCode);
+  }
+
+  @Output('modColor')
+  coloChange = new EventEmitter<String>();
+  onModify(colorCode:string) {
+    this.color = colorCode;
   }
 }
